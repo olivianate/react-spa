2cc13e1f21a428f04c9064b0ed678dc37662203b
olivianate <1061691060@qq.com>
2018-06-28 16:04:29 +0800

    change host

--- src/config/index.js
+++ src/config/index.js
@@ -7,8 +7,8 @@ const dev = {
 const pro = {
   isMock: true,
   isDebug: false,
-  // host:'',
-  host: 'react-spa',
+  host:'',
+  // host: 'react-spa',
 };
 
 const config = process.env.NODE_ENV === 'production' ? pro : dev;
